0fd4369b8911babf33a701a7cdd658da45bbe5ec
ujjval.priyadarshi <ujjval.priyadarshi@lybrate.com>
2023-09-27 00:10:12 +0530

    Changing meaning full url

--- utility/blogMapping.tsx
+++ utility/blogMapping.tsx
@@ -3,7 +3,7 @@ const blogs = [
     name: 'How to center a div',
     description:
       "There are so many methods to center a <div />, let's see how many do you know",
-    url: '/blog/center-div',
+    url: '/blog/how-to-center-a-div',
     topic: ['HTML/CSS', 'Technical Knowledge'],
     imageUrl: '/blogs/center-div-css.webp',
   },
@@ -11,7 +11,7 @@ const blogs = [
     name: 'How to use Debounce with custom hook in Reactjs',
     description:
       "I love making custom hook, it's make my life so much easy and very easy to implement. In this blog I will tell how easily you can implement debounce to any input element using a custom hook",
-    url: '/blog/debounce-hook',
+    url: '/blog/use-debounce-with-custom-hook',
     topic: ['React', 'React Hook', 'Technical Knowledge'],
     imageUrl: '/blogs/react-hooks.jpg',
   },
