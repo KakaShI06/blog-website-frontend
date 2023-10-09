7c968d25c69504a5500af943750485af45bff3a4
ujjval.priyadarshi <ujjval.priyadarshi@lybrate.com>
2023-10-10 00:10:59 +0530

    Adding link

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
+import BlogCard from '@/components/BlogCard'
 import FilteredBlogList from '@/components/FilteredBlogList'
-import LatestPost from '@/components/LatestPost'
 import Layout from '@/components/Layout'
 import WelcomeScreen from '@/components/WelcomeScreen'
 import blogs from '@/utility/blogMapping'
@@ -8,8 +8,19 @@ export default function Home() {
   return (
     <Layout>
       <WelcomeScreen />
-      <LatestPost blog={blogs[blogs.length - 1]} />
-      <h2 className='text-2xl font-bold p-4 pl-0 underline'>Checkout My Other Blogs: </h2>
+      <h1 className='font-bold text-2xl underline pt-2 pb-4 mt-4'>
+        Checkout My Latest Blog Here:
+      </h1>
+      <BlogCard
+        name={blogs[blogs.length - 1].name}
+        description={blogs[blogs.length - 1].description}
+        imageUrl={blogs[blogs.length - 1].imageUrl}
+        topics={blogs[blogs.length - 1].topic}
+        link={blogs[blogs.length - 1].url}
+      />
+      <h2 className='text-2xl font-bold p-4 pl-0 underline'>
+        Checkout My Other Blogs:{' '}
+      </h2>
       <FilteredBlogList blogs={blogs} />
     </Layout>
   )
--- utility/blogMapping.tsx
+++ utility/blogMapping.tsx
@@ -15,6 +15,14 @@ const blogs = [
     topic: ['React', 'React Hook', 'Technical Knowledge'],
     imageUrl: '/blogs/react-hooks.jpg',
   },
+  {
+    name: 'How to use Debounce with custom hook in Reactjs',
+    description:
+      "I always had a hard time in understanding the webpack concept. Since the all the automated packages out there in market you really don't even need to understand in the early stage of your career, but if you really want to understand how the things work under the hood Module Bundler is really important.",
+    url: '/blog/module-bundler',
+    topic: ['Technical Knowledge', 'Webpack'],
+    imageUrl: '/blogs/webpack.png',
+  }
 ]
 
 export default blogs
